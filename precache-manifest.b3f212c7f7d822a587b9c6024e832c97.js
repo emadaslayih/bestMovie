e1984a545414c5d5e5896cf14a7b71e8b7dda2c3
emadaslayih <emad-slayeh@windowslive.com>
2020-06-19 14:58:52 +0300

    bestMovie2.0

---
+++ precache-manifest.b3f212c7f7d822a587b9c6024e832c97.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63f02a221a471eee634c2979bc7121f",
+    "url": "/bestMovie/index.html"
+  },
+  {
+    "revision": "d5e7aa61b835c5de163c",
+    "url": "/bestMovie/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "73161486da747b1db57f",
+    "url": "/bestMovie/static/css/main.20886b82.chunk.css"
+  },
+  {
+    "revision": "d5e7aa61b835c5de163c",
+    "url": "/bestMovie/static/js/2.ebc3137a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bestMovie/static/js/2.ebc3137a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73161486da747b1db57f",
+    "url": "/bestMovie/static/js/main.85fb5397.chunk.js"
+  },
+  {
+    "revision": "a6c5bdc50ae941cf3cca",
+    "url": "/bestMovie/static/js/runtime-main.b15d7a52.js"
+  },
+  {
+    "revision": "cf8964ef57378199972817c26fef5ad2",
+    "url": "/bestMovie/static/media/scoob.cf8964ef.jpg"
+  },
+  {
+    "revision": "e0bf5d210c5fa9f1fc5fb2601d4ef56d",
+    "url": "/bestMovie/static/media/tinTin.e0bf5d21.jpg"
+  }
+]);
